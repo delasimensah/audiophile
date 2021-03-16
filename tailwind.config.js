f5bc792c71b99502fbbdd4e5199bf8221aa6f324
delasimensah <mensadelasi@gmail.com>
2021-03-16 16:30:25 +0000

    added colors and dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,24 @@
 module.exports = {
-  mode:'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {}
+      colors: {
+        primary: "#D87D4A",
+        primaryLt: "#FBAF85",
+        gray: "#F1F1F1",
+        grayLt: "#FAFAFA",
+        dark: "#101010",
+      },
     },
+    // backgroundImage: () => ({
+    //   home: "url('/images/home-bg.jpg')",
+    // }),
   },
   variants: {
     extend: {},
   },
   plugins: [],
-  important: true
-}
+  important: true,
+};
