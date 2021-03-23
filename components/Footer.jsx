b87874dfaa06325e52feb1fa1a4e0fe02d0caf28
delasimensah <mensadelasi@gmail.com>
2021-03-23 16:30:25 +0000

    added footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import {
+  IoLogoFacebook,
+  IoLogoInstagram,
+  IoLogoTwitter,
+} from "react-icons/io5";
+
+//mui
+import { Typography } from "@material-ui/core";
 
 const Footer = () => {
-  return <div className="h-20 text-white bg-dark">footer</div>;
+  return (
+    <div className="grid gap-10 text-white padding-h bg-dark lg:grid-cols-2">
+      <div className="flex flex-col items-center space-y-10 md:block">
+        <div className="h-[4px] w-28 bg-primary"></div>
+        <img src="/assets/shared/desktop/logo.svg" alt="" />
+      </div>
+
+      <div className="lg:pt-10 lg:flex lg:justify-end">
+        <ul className="flex flex-col items-center justify-center space-y-5 text-sm tracking-widest uppercase md:justify-start md:flex-row md:space-x-5 md:space-y-0">
+          <li>home</li>
+          <li>headphones</li>
+          <li>speakers</li>
+          <li>earphones</li>
+        </ul>
+      </div>
+
+      <div>
+        <Typography className="text-sm leading-relaxed tracking-wider text-center font-extralight md:text-left">
+          Audiophile is an all in one stop to fulfill your audio needs. We're a
+          small team of music lovers and sound specialists who are devoted to
+          helping you get the most out of personal audio. Come and visit our
+          demo facility - we’re open 7 days a week.
+        </Typography>
+      </div>
+
+      <div className="hidden lg:flex lg:justify-end lg:items-end lg:space-x-3">
+        <IoLogoFacebook className="cursor-pointer w-7 h-7 hover:text-primary" />
+        <IoLogoTwitter className="cursor-pointer w-7 h-7 hover:text-primary" />
+        <IoLogoInstagram className="cursor-pointer w-7 h-7 hover:text-primary" />
+      </div>
+
+      <div className="pb-10 space-y-10 md:space-y-0 md:flex lg:block">
+        <div className="flex-grow">
+          <Typography className="text-sm tracking-wider text-center font-extralight md:text-left">
+            Copyright {new Date().getFullYear()}. All Rights Reserved
+          </Typography>
+        </div>
+
+        <div className="flex justify-center space-x-3 md:justify-end lg:hidden">
+          <IoLogoFacebook className="w-7 h-7" />
+          <IoLogoTwitter className="w-7 h-7" />
+          <IoLogoInstagram className="w-7 h-7" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ components/Info.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div className="grid padding-h lg:grid-cols-2">
+      <div>
+        Bringing you the best audio gear Located at the heart of New York City,
+        Audiophile is the premier store for high end headphones, earphones,
+        speakers, and audio accessories. We have a large showroom and luxury
+        demonstration rooms available for you to browse and experience a wide
+        range of our products. Stop by our store to meet some of the fantastic
+        people who make Audiophile the best place to buy your portable audio
+        equipment.
+      </div>
+
+      <div>image</div>
+    </div>
+  );
+};
+
+export default Info;
